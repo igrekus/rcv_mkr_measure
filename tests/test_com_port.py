e20643b320f51fc5378d4b096b268e2700dd8d36
igrekus <upload234@mail.ru>
2019-02-28 18:54:32 +0300

    REF: rename serial_obj for simplicity

--- tests/test_com_port.py
+++ tests/test_com_port.py
@@ -37,18 +37,18 @@ class SerialMock:
 
 def test_com_port_init():
 
-    serial_obj = com_port_init(serial_obj=SerialMock())
-    serial_obj.open()
-    serial_obj.write(b'$KE,IO,SET,7,0\r\n')
-    ans1 = serial_obj.read_all()
-    serial_obj.write(b'$KE,WRA,000000010101010000000000\r\n')
-    ans2 = serial_obj.read_all()
+    s = com_port_init(serial_obj=SerialMock())
+    s.open()
+    s.write(b'$KE,IO,SET,7,0\r\n')
+    ans1 = s.read_all()
+    s.write(b'$KE,WRA,000000010101010000000000\r\n')
+    ans2 = s.read_all()
 
-    expect(serial_obj.is_open).to_equal(True)
+    expect(s.is_open).to_equal(True)
     expect(ans1).to_equal(b'#OK\r\n')
     expect(ans2).to_equal(b'#OK\r\n')
 
-    serial_obj.close()
+    s.close()
 
-    expect(serial_obj.is_open).to_equal(False)
+    expect(s.is_open).to_equal(False)
 
