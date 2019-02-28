346f07705553751ab725b49c0a4f169e8d104f0e
igrekus <upload234@mail.ru>
2019-02-28 18:19:21 +0300

    WIP: update test for com_port_init()

--- tests/test_com_port.py
+++ tests/test_com_port.py
@@ -1,12 +1,13 @@
-import pyexpect
+from pyexpect import expect
 from rewrite import com_port, com_port_init
 
 
 class SerialMock:
+
     def __init__(self, port='COM1', baudrate=115200, parity='N', bytesize=8, stopbits=1, timeout=0.2):
         self._port = port
         self._open = False
-        self._last_write = ''
+        self._last_write = b''
         self._success = b'#OK\r\n'
 
     def open(self):
@@ -19,14 +20,14 @@ class SerialMock:
         self._last_write = what
 
     def read_all(self):
-        ans = ''
-        self._last_write = ''
+        ans = b''
         if self._last_write == b'$KE\r\n':
             ans = self._success
         elif b'$KE,IO,SET,' in self._last_write:
             ans = self._success
         elif b'$KE,WRA,' in self._last_write:
             ans = self._success
+        self._last_write = b''
         return ans
 
     @property
@@ -36,4 +37,18 @@ class SerialMock:
 
 def test_com_port_init():
 
-    serial_obj = com_port_init('COM4')
+    serial_obj = com_port_init(serial_obj=SerialMock())
+    serial_obj.open()
+    serial_obj.write(b'$KE,IO,SET,7,0\r\n')
+    ans1 = serial_obj.read_all()
+    serial_obj.write(b'$KE,WRA,000000010101010000000000\r\n')
+    ans2 = serial_obj.read_all()
+
+    expect(serial_obj.is_open).to_equal(True)
+    expect(ans1).to_equal(b'#OK\r\n')
+    expect(ans2).to_equal(b'#OK\r\n')
+
+    serial_obj.close()
+
+    expect(serial_obj.is_open).to_equal(False)
+
