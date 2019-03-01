31197a036099d22cab44feb2345fda126bec8d5b
igrekus <upload234@mail.ru>
2019-03-01 16:55:44 +0300

    MISC: fix encoding, reformat funcrion

--- matlab/com_port_close.m
+++ matlab/com_port_close.m
@@ -1,7 +1,6 @@
-%закрытие порта USB-модуля
 function com_port_close(serial_obj)
-    %fprintf('закрытие COM-порта\n');
-    %fprintf(serial_obj, '%s\r\n','$KE,WRA,000000000000000000000000');
-    fclose(serial_obj) ;
+    % Р·Р°РєСЂС‹С‚РёРµ РїРѕСЂС‚Р° USB-РјРѕРґСѓР»СЏ
+    fclose(serial_obj);
     delete(serial_obj);
     clear serial_obj;
+end;
