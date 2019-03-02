fedd2ce660727ea1d982a62833161d2569ba7288
igrekus <upload234@mail.ru>
2019-03-02 16:44:58 +0300

    REF: use mock for test

--- tests/test_receiver_control.py
+++ tests/test_receiver_control.py
@@ -7,7 +7,8 @@ def test_receiver_control_with_bitstring_0():
 
     s = SerialMock()
 
-    ans = receiver_control(bit_str='0', state=0, serial_obj=s)
+    ans = receiver_control(bit_str='0', state=0,
+                           serial_obj=SerialMock())
 
     expect(ans).to_equal('init complete')
 
