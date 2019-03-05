d5794f7304047208d06d6da6d678dade6503bbd9
igrekus <upload234@mail.ru>
2019-03-05 15:30:26 +0300

    REF: fix test for PNA init logic

--- tests/test_init_pna.py
+++ tests/test_init_pna.py
@@ -1,11 +1,11 @@
 from pyexpect import expect
 from pnamock import PnaMock
-from rewrite import init_pna
+from rewrite import pna_init
 
 
 def test_init_pna():
 
-    pna, err = init_pna(1, PnaMock())
+    pna, err = pna_init(1, PnaMock())
 
     expect(isinstance(pna, PnaMock)).to_equal(True)
     expect(err).to_equal(0)
