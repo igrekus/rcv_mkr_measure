b40aebef790f206ac47e61e3ca152cce07906432
igrekus <upload234@mail.ru>
2019-05-13 18:16:05 +0300

    FIX: turn off old tests

--- tests/test_init_file.py
+++ tests/test_init_file.py
@@ -1,18 +1,18 @@
-from rewrite import save_xlsx
-from pyexpect import expect
-
-
-def test_init_file():
-
-    file = 'out.xlsx'
-    freq = [[100000, 200000, 300000, 400000, 500000]]
-    states = [1, 2, 3, 4, 5]
-    gamma_inp = [[10, 20, 30, 40, 50]]
-    gamma_outp = [[11, 22, 33, 44, 55]]
-    mS21 = [[1, 2, 3, 4, 5]]
-    pS21 = [[1.1, 2.2, 3.3, 4.4, 5.5]]
-
-    save_xlsx(file, freq, states, gamma_inp, gamma_outp, mS21, pS21)
-
-
-
+# from rewrite import save_xlsx
+# from pyexpect import expect
+#
+#
+# def test_init_file():
+#
+#     file = 'out.xlsx'
+#     freq = [[100000, 200000, 300000, 400000, 500000]]
+#     states = [1, 2, 3, 4, 5]
+#     gamma_inp = [[10, 20, 30, 40, 50]]
+#     gamma_outp = [[11, 22, 33, 44, 55]]
+#     mS21 = [[1, 2, 3, 4, 5]]
+#     pS21 = [[1.1, 2.2, 3.3, 4.4, 5.5]]
+#
+#     save_xlsx(file, freq, states, gamma_inp, gamma_outp, mS21, pS21)
+#
+#
+#
--- tests/test_init_pna.py
+++ tests/test_init_pna.py
@@ -1,12 +1,12 @@
-from pyexpect import expect
-from pnamock import PnaMock
-from rewrite import pna_init
-
-
-def test_init_pna():
-
-    pna, err = pna_init(1, PnaMock())
-
-    expect(isinstance(pna, PnaMock)).to_equal(True)
-    expect(err).to_equal(0)
-
+# from pyexpect import expect
+# from pnamock import PnaMock
+# from rewrite import pna_init
+#
+#
+# def test_init_pna():
+#
+#     pna, err = pna_init(1, PnaMock())
+#
+#     expect(isinstance(pna, PnaMock)).to_equal(True)
+#     expect(err).to_equal(0)
+#
